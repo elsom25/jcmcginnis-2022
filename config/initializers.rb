742017267170a992c02c6b775b0a8d16b476db7b
Jesse McGinnis <accounts@jcmcginnis.com>
2025-12-20 16:00:22 -0500

    /deslop

--- config/initializers.rb
+++ config/initializers.rb
@@ -1,12 +1,10 @@
 # frozen_string_literal: true
 
 Bridgetown.configure do |config|
-  # Initialize plugins
   init :"bridgetown-feed"
   init :"bridgetown-seo-tag"
 end
 
-# Custom template helpers
 Bridgetown::RubyTemplateView::Helpers.class_eval do
   def featured_post
     home_posts = site.collections.posts.resources.select { |p| p.data.home }
@@ -26,8 +24,6 @@ Bridgetown::RubyTemplateView::Helpers.class_eval do
     site.collections.posts.resources.group_by { |p| p.date.year }.sort.reverse.to_h
   end
 
-  # Look up category page by title and return its URL (single source of truth)
-  # Falls back to a slugified URL if no matching page is found
   def category_url(category_name)
     page = site.collections.pages.resources.find { |p| p.data.title == category_name }
     page&.relative_url || "/category/#{category_name.downcase.gsub(/\s+/, '-')}/"
